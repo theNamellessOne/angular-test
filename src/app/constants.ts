3fdf3ad07799c07f2b2dbc4ad541b751a4dd2430
theNamellessOne <145769204+theNamellessOne@users.noreply.github.com>
2023-09-26 14:51:04 +0300

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  'http://ds.cargo.kdg.com.ua:9998/SKExpress/SKExpress/Backend/';
+  'https://cargoexpress.kdg.com.ua:9999/SKExpress/SKExpress/Backend/';
 export const AUTH_TOKEN =
   'Basic c2tleHByZXNzJTdDdGVzdEB0ZXN0LmNvbToxMTExMTExMQ==';
